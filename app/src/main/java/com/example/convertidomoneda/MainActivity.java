02f0a710a99489a1e126227fc729ae9946198464
gabriel <aballaygabriel@gmail.com>
2019-09-04 22:35:14 -0300

    Initial commit

---
+++ app/src/main/java/com/example/convertidomoneda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.convertidomoneda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
